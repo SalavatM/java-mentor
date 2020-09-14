df9bb4efe984b80666d0235c7ac132b170e17d00
SalavatM <techprosm@yandex.ru>
2020-09-14 16:11:09 +0300

    Консольное приложение Калькулятор: операции сложения, вычитания, умножения и деления с арабскими и римскими числами от 1 до 10.

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -1,14 +1,17 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Calculator {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         String str = getStr();
-        int result = calc(str);
+        String result = calc(str);
         System.out.println("Результат операции: " + result);
     }
 
@@ -25,8 +28,8 @@ public class Calculator {
         return str;
     }
 
-    public static int calc(String str){
-        int result;
+    public static String calc(String str){
+        String result;
 
         int index1 = str.indexOf('+'); // операция сложения
         int index2 = str.indexOf('-'); // операция вычитания
@@ -45,15 +48,18 @@ public class Calculator {
         boolean num1isArabic = Arrays.asList(aNumbers).contains(strnum1);
         boolean num2isArabic = Arrays.asList(aNumbers).contains(strnum2);
         boolean num1isRoman = Arrays.asList(rNumbers).contains(strnum1);
-        boolean num2isRoman = Arrays.asList(rNumbers).contains(strnum1);
+        boolean num2isRoman = Arrays.asList(rNumbers).contains(strnum2);
+
+        boolean isArabic = num1isArabic && num2isArabic;
+        boolean isRoman = num1isRoman && num2isRoman;
 
         int num1 = 0, num2 = 0;
-        if ((num1isArabic & num2isArabic) | (num1isRoman & num2isRoman)) {
-            if (num1isArabic & num2isArabic) {
+        if (isArabic || isRoman) {
+            if (isArabic) {
                 num1 = Integer.parseInt(strnum1);
                 num2 = Integer.parseInt(strnum2);
             }
-            if (num1isRoman & num2isRoman) {
+            if (isRoman) {
                 num1 = Arrays.asList(rNumbers).indexOf(strnum1) + 1;
                 num2 = Arrays.asList(rNumbers).indexOf(strnum2) + 1;
             }
@@ -63,26 +69,77 @@ public class Calculator {
             return result;
         }
 
+        int res = 0;
         switch (operation){
             case '+':
-                result = num1 + num2;
+                res = num1 + num2;
                 break;
             case '-':
-                result = num1 - num2;
+                res = num1 - num2;
                 break;
             case '*':
-                result = num1 * num2;
+                res = num1 * num2;
                 break;
             case '/':
-                result = num1 / num2;
+                res = num1 / num2;
                 break;
             default:
                 System.out.println("Операция не распознана. Повторите ввод.");
                 result = calc(getStr());
         }
 
+        result = ((Integer)res).toString();
+        if (isRoman && res > 0) {
+            result = NumeralsConversion.arabicToRoman(res);
+        }
+
         return result;
     }
 }
 
+class NumeralsConversion {
+    enum RomanNumeral {
+        I(1), IV(4), V(5), IX(9), X(10),
+        XL(40), L(50), XC(90), C(100);
+
+        private int value;
+
+        RomanNumeral(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public static List<RomanNumeral> getReverseSortedValues() {
+            return Arrays.stream(values())
+                    .sorted(Comparator.comparing((RomanNumeral e) -> e.value).reversed())
+                    .collect(Collectors.toList());
+        }
+    }
+
+    public static String arabicToRoman(int number) {
+        if ((number <= 0) || (number > 100)) {
+            throw new IllegalArgumentException(number + " не входит в диапазон чисел от 1 до 100");
+        }
+
+        List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
+
+        int i = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while ((number > 0) && (i < romanNumerals.size())) {
+            RomanNumeral currentSymbol = romanNumerals.get(i);
+            if (currentSymbol.getValue() <= number) {
+                sb.append(currentSymbol.name());
+                number -= currentSymbol.getValue();
+            } else {
+                i++;
+            }
+        }
+
+        return sb.toString();
+    }
+}
 
